74c36f5894835d888afd508a84c9e957ec91ea5e
FefilovaVv <131516275+FefilovaVv@users.noreply.github.com>
2023-08-04 20:07:33 +0700

    Update js.js

--- js.js
+++ js.js
@@ -2,40 +2,85 @@ document.addEventListener("DOMContentLoaded", onPageLoaded);
 
 
 function onPageLoaded() {
-  getPointsFromStorage();
-
+  
+  const titlesContainer =document.querySelector(".titles-container");
   const pointsList = document.querySelector(".points-list");
   const textReseiver = document.querySelector('.text-reseiver');
   const scoreReseiver = document.querySelector('.score-reseiver');
   const addBtn = document.querySelector(".add-btn");
   const sortBtn = document.querySelector(".sort-btn");
-  const allBtn = document.querySelectorAll("button");
+  const allBtn = document.querySelectorAll("button","h3");
+  const btnAddMenuTitle = document.querySelector('.btn-add-menu-title');
+  btnAddMenuTitle.addEventListener('click', addMenu);
+  let targetTitle = document.querySelector('.menu-title');
+  let menuTitle = document.querySelector('h3');
 
+    
 
+  
   scoreReseiver.addEventListener('change', showRangeResult)
   addBtn.addEventListener('click', addPoint);
   sortBtn.addEventListener('click', sortPoint);
   for (let button of allBtn) {button.addEventListener('click', putPointsInStorage)};
-
-
+  
+  putPointsInStorage();
+  
   function putPointsInStorage(){
   let pointsListContent = pointsList.innerHTML;
-    localStorage.setItem('pointsListStored', JSON.stringify(pointsListContent));};
+    localStorage.setItem('pointsListStored', JSON.stringify(pointsListContent));
+    
+    let titlesContainerContent = titlesContainer.innerHTML;
+    localStorage.setItem('titlesContainerStored', JSON.stringify(titlesContainerContent));
+  
+    
+   targetTitle.dataset.menuContent = pointsList.innerHTML;
+    console.log(titlesContainer.innerHTML);
+    console.log(titlesContainerContent);
+  };
     
   function getPointsFromStorage() {
-    
     const pointsList = document.querySelector(".points-list");
     const pointsListStored = JSON.parse(localStorage.getItem('pointsListStored'));
     pointsList.innerHTML = pointsListStored;
+    const titlesContainer = document.querySelector(".titles-container");
+    const titlesContainerStored = JSON.parse(localStorage.getItem('titlesContainerStored'));
+    titlesContainer.innerHTML = titlesContainerStored;
     
     const allDeleteBtn = document.querySelectorAll('.point-delete');
     for (let button of allDeleteBtn) { button.addEventListener('click', deletePoint); };
+    allMenuTitles = document.querySelectorAll('.menu-title');
+    for (let title of allMenuTitles) {title.addEventListener('click', activateMenu)};
+
     
   };
 
   function showRangeResult() {
     const rangeResult = document.querySelector('.range-result')
     rangeResult.innerText = scoreReseiver.value;
+  };
+  
+  function addMenu() {
+    menuTitle = document.createElement('h3');
+    menuTitle.classList.add('menu-title');
+    menuTitle.setAttribute('contenteditable', 'true');
+    menuTitle.textContent = 'Название';
+    const deleteBtn = document.createElement("button");
+    
+    deleteBtn.classList.add("point-delete");
+    deleteBtn.textContent = "×";
+    menuTitle.append(deleteBtn);
+    titlesContainer.append(menuTitle);
+    allMenuTitles = document.querySelectorAll('.menu-title');
+    for (let title of allMenuTitles) {title.addEventListener('click', activateMenu)};
+
+  };
+  
+  function activateMenu(){
+    targetTitle = event.target;
+    const pointsList = document.querySelector(".points-list");
+    targetTitle.dataset.menuContent === undefined ?pointsList.innerHTML = '':pointsList.innerHTML=targetTitle.dataset.menuContent ;
+    
+    return targetTitle;
   };
 
   function addPoint() {
@@ -67,4 +112,12 @@ function onPageLoaded() {
   function deletePoint() {
     this.parentNode.remove();
   };
+  
+  
+  
+    
+  
+  
+  
+
 };
