798869f7c1e4b436655d6af7b9e6894847b4cc13
FefilovaVv <131516275+FefilovaVv@users.noreply.github.com>
2023-08-12 21:55:14 +0700

    Update js.js

--- js.js
+++ js.js
@@ -10,9 +10,11 @@ function onPageLoaded() {
     const scoreReseiver = document.querySelector('.score-reseiver');
     const sortBtn = document.querySelector(".sort-btn");
     const allBtn = document.querySelectorAll("button", "h3");
-
     
-    scoreReseiver.addEventListener('change', showRangeResult);
+    textReseiver.addEventListener('keydown', ()=>{
+      event.key==='Enter'?addPoint():null;
+    })
+    scoreReseiver.addEventListener('input', showRangeResult);
     sortBtn.addEventListener('click', sortPoint);
     for (let button of allBtn) { button.addEventListener('click', putPointsInStorage) };
 
@@ -23,15 +25,13 @@ function onPageLoaded() {
         localStorage.setItem('pointsListStored', JSON.stringify(pointsListContent));
         localStorage.setItem('titlesContainerStored', JSON.stringify(titlesContainerContent));
         targetTitle.dataset.menuContent = pointsList.innerHTML;
-        
-        console.log(targetTitle)
     };
 
     function activateMenu() {
-        targetTitle = event.target;
+        targetTitle = this;
         const pointsList = document.querySelector(".points-list");
         targetTitle.dataset.menuContent === undefined ? pointsList.innerHTML = '' : pointsList.innerHTML = targetTitle.dataset.menuContent;
-console.log(targetTitle)
+
         return targetTitle;
     };
 
@@ -43,11 +43,12 @@ console.log(targetTitle)
 
         pointsList.innerHTML = pointsListStored;
         titlesContainerStored !== null ? titlesContainer.innerHTML = titlesContainerStored : (titlesContainer.innerHTML =
-            `<h3 class="btn-add-title-menu">+</h3>
-            <h3 class="menu-title" contenteditable="" data-menu-content="">Меню <button class="point-delete">×</button></h3>`
+            `<h3 class="btn-add-title">+</h3><div class="a-title-container">
+            <h3 class="menu-title first" contenteditable="" data-menu-content="">Меню</h3><button class="point-delete">×</button>
+</div>`
         );
 
-        const btnAddMenuTitle = document.querySelector('.btn-add-title-menu');
+        const btnAddMenuTitle = document.querySelector('.btn-add-title');
         const addBtn = document.querySelector(".add-btn");
         const allDeleteBtn = document.querySelectorAll('.point-delete');
         const allMenuTitles = document.querySelectorAll('.menu-title');
@@ -68,24 +69,27 @@ console.log(targetTitle)
         menuTitle.classList.add('menu-title');
         menuTitle.setAttribute('contenteditable', 'true');
         menuTitle.textContent = 'Название';
+        const aTitleContainer = document.createElement('div')
+        aTitleContainer.classList.add('a-title-container');
 
         const deleteBtn = document.createElement("button");
         deleteBtn.classList.add("point-delete");
         deleteBtn.setAttribute('contenteditable', 'false');
         deleteBtn.textContent = "×";
-
-        menuTitle.append(deleteBtn);
-        titlesContainer.append(menuTitle);
+        
+        aTitleContainer.append(menuTitle, deleteBtn);
+        titlesContainer.append(aTitleContainer);
 
         menuTitle.addEventListener('click', activateMenu);
         deleteBtn.addEventListener('click', deletePoint);
       
-        activateMenu.call(document.querySelector('.menu-title:last-child'));
-        
+        activateMenu.call(document.querySelector('.a-title-container:last-child .menu-title'));
+
     };
 
 
     function addPoint() {
+        const allMenuTitles = document.querySelectorAll('.menu-title');if( allMenuTitles.length>0){
         const pointContainer = document.createElement("li");
         pointContainer.classList.add("point-container");
         const pointText = document.createElement("span");
@@ -96,12 +100,14 @@ console.log(targetTitle)
         deleteBtn.classList.add("point-delete");
 
         pointText.textContent = textReseiver.value;
-        textReseiver.value = '';
         pointScore.textContent = scoreReseiver.value;
         deleteBtn.textContent = "×";
+        textReseiver.value ="";
+        
 
         pointsList.appendChild(pointContainer).append(pointText, pointScore, deleteBtn);
         deleteBtn.addEventListener('click', deletePoint);
+        };
     };
 
     function sortPoint() {
@@ -116,9 +122,7 @@ console.log(targetTitle)
         this.parentNode.remove();
         event.stopPropagation();
         const allMenuTitles = document.querySelectorAll('.menu-title');
-        console.log(allMenuTitles.length)
+        allMenuTitles.length===0?pointsList.innerHTML=`<h2 class ="tip-for-creating-menu">Сначала тыкни на +, чтобы создать меню</h2>`: null;
+
     };
-
-    
-
 };
